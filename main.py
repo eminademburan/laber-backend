957dc23240684951bfb76d4752950a5dbc9fc340
DESKTOP-J6PID05\adems <ademsan99@gmail.com>
2022-03-12 11:03:41 +0300

    answering question is completed

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS, cross_origin
 from flask_pymongo import PyMongo
-import pandas as pd
+
+
+import time
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+#define the function whic will be scheduled
+
+#scheduler = BackgroundScheduler()
+#scheduler.add_job(func=print_date_time, trigger="interval", seconds=20)
+#scheduler.start()
 
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb+srv://ademburan:proje1234@cluster0.9k20l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
@@ -10,21 +21,10 @@ db = mongo.db
 CORS(app)
 api = Api(app)
 
-incomes = [{"amount" : 5000}]
-
-
-
-
-@app.route('/incomes')
+@app.route("/message")
 @cross_origin()
-def get_incomes():
-    return jsonify(incomes)
-
-@app.route("/add_one")
-@cross_origin()
-def add_one():
-    db.todos.insert_one({'title': "todo title", 'body': "todo body"})
-    return jsonify(message="success")
+def get_user2():
+    return jsonify(message="hello")
 
 @app.route("/add_user/<string:email>/<string:name>/<string:surname>/<string:phone>/<int:age>/<string:region>/<string:language>/<string:password>/<int:invlink>")
 @cross_origin()
@@ -34,6 +34,76 @@ def add_user(email,name,surname,phone,age,region,language,password,invlink):
         return jsonify(message="success")
     except:
         return jsonify(message="failed")
+
+
+@app.route("/add_tweet/<int:tweet_id>/<string:text>/<int:noOfLike>/<string:tweetGroup>")
+@cross_origin()
+def add_tweet(tweet_id,text,noOfLike,tweetGroup):
+    try:
+        db.tweets.insert_one({ '_id': tweet_id, 'text': text, 'likes': noOfLike, 'owner_id': tweetGroup })
+        return jsonify(message="success")
+    except:
+        return jsonify(message="failed")
+
+@app.route("/get_tweet/<int:tweet_id>")
+@cross_origin()
+def get_tweet(tweet_id):
+    try:
+        query = {'_id': tweet_id}
+        tweet = db.tweets.find_one(query)
+        if tweet is None:
+            return jsonify(None)
+        else:
+            return jsonify(tweet)
+    except:
+        return jsonify(None)
+
+
+
+@app.route("/add_response/<int:tweet_id>/<string:responser>/<string:sentiment>/<string:sarcasm>")
+@cross_origin()
+def add_response(tweet_id, responser, sentiment, sarcasm):
+    try:
+        query = { 'status': "Waiting", 'tweet_id': tweet_id, 'responser' :responser }
+        if db.answers.find_one( query) is None:
+            return jsonify(message="failed")
+        else:
+            query = { 'tweet_id': tweet_id, 'responser' : responser }
+            new_values = { "$set": { 'sentiment': sentiment, 'sarcasm': sarcasm, 'status': 'Answered'}}
+            db.answers.update_one( query, new_values)
+            return jsonify(message="true")
+    except:
+        return jsonify(message="failed")
+
+@app.route("/get_tweet_to_answer/<string:responser>")
+@cross_origin()
+def getTweet2(responser):
+    try:
+        query = { 'responser': responser, 'status': 'Waiting' }
+        projection = { '_id':0, 'tweet_id':1}
+        tweet = db.answers.find_one( query, projection)
+        if tweet is None:
+            return jsonify(None)
+        else:
+            return jsonify(tweet)
+    except:
+        return jsonify(None)
+
+@app.route("/assign_user/<int:tweet_id>/<string:responser>")
+@cross_origin()
+def assignUser(tweet_id, responser):
+    try:
+        query = { 'tweet_id': tweet_id, 'responser' : responser }
+        if db.answers.find_one( query) is not None:
+            return jsonify(message="failed")
+        else:
+            db.answers.insert_one(
+                {'tweet_id': tweet_id, 'responser': responser, 'sentiment': '', 'sarcasm': '',
+                 'status': 'Waiting'})
+            return jsonify(message="success")
+    except:
+        return jsonify(message="failed")
+
 
 @app.route("/get_user/<string:email>")
 @cross_origin()
@@ -45,29 +115,7 @@ def get_user(email):
         else:
             return jsonify(userWithPassword)
     except:
-        return jsonify(message="failed")
-
-
-@app.route("/add_many")
-@cross_origin()
-def add_many():
-    db.todos.insert_many([
-        {'_id': 1, 'title': "todo title one ", 'body': "todo body one    "},
-        {'_id': 2, 'title': "todo title two", 'body': "todo body two"},
-        {'_id': 3, 'title': "todo title three", 'body': "todo body three"},
-        {'_id': 4, 'title': "todo title four", 'body': "todo body four"},
-        {'_id': 5, 'title': "todo title five", 'body': "todo body five"},
-        ])
-    return jsonify(message="success")
-
-
-
-@app.route("/get_todo/<int:todoId>")
-@cross_origin()
-def insert_one(todoId):
-    todo = db.todos.find_one({"_id": todoId})
-    return jsonify(todo)
-
+        return jsonify(None)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
