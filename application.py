735cf81c842f57d80869454ea5f5128884582e89
Mustafa Hakan Kara <mustafahakankara@hotmail.com>
2022-05-07 23:40:21 +0300

    mess up aws temporarily

--- application.py
+++ application.py
@@ -31,7 +31,7 @@ load_dotenv()
 application.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(application)
 mongo = PyMongo(application,
-                uri="mongodb+srv://ademburan:proje1234@cluster0.9k20l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+                uri="mongodb+srv://aademburan:proje1234@cluster0.9k20l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = mongo.db
 CORS(application)
 api = Api(application)
