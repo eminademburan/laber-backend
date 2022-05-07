44eaa80ee46e8153a51315c7e3aac4ccf6081ec1
Mustafa Hakan Kara <mustafahakankara@hotmail.com>
2022-05-07 16:47:58 +0300

    check scalars for voice chat

--- crosscheck.py
+++ crosscheck.py
@@ -19,8 +19,9 @@ import atexit
 
 import voicechat
 
-from apscheduler.schedulers.background import BackgroundScheduler
+import numpy as np
 
+from apscheduler.schedulers.background import BackgroundScheduler
 
 application = Flask(__name__)
 load_dotenv()
@@ -33,12 +34,11 @@ CORS(application)
 api = Api(application)
 
 
-def z_score(X):
-    M = sum(X) / len(X)
-    diff = [(x-M) for x in X]
-    std = (sum([x**2 for x in diff]) / len(X))**(1/2)
-    return [x/std for x in diff]
-
+def z_score(answers):
+    answers = answers.T
+    mean = np.mean(answers, axis=1, keepdims=True)
+    std = np.std(answers, axis=1, keepdims=True)
+    return (answers - mean) / std
 
 def get_metrics(task_id):
     query = {"_id": task_id}
@@ -52,16 +52,36 @@ def get_answers(tweet_id):
     return db.answers.find(query, projection)
 
 
-def check_nonscalar(metrics, answers):
-    pass
+ROOM_SIZE = 5
 
+def check_nonscalar(answers):
 
-def check_scalar(metrics, answers):
-    n = len(metrics)
-    for i in range(n):
-        min = metrics[i]["min"]
-        max = metrics[i]["max"]
+def check_scalar(answers):
+    # all the answers for all the metrics of a tweet are given
+    # rows are experts, and columns are answers
+    # 1) compute the z-scores of the answers
+    # 2) check if any outliers (z-scores between (-inf, -1) and (1, inf))
+    # 3) choose <room_size> experts by the following rule:
+    #    choose the 2 from leftmost, 2 from rightmost and 1 from middle
+    #    if <room_size> is 5
 
+    # compute the z-scores:
+    z_scores = z_score(answers)
+    mins = np.min(z_scores, axis=1)
+    maxes = np.max(z_scores, axis=1)
+
+    # No need for a voice chat for metrics without outliers
+    satisfied = np.logical_or(np.less(mins, -1), np.greater(maxes, 1))
+    # satisfying_rows = np.flatnonzero(satisfied)
+
+    sorted_experts = np.argsort(z_scores, axis=1)
+    leftmosts = sorted_experts[ROOM_SIZE//2:]
+    middle = sorted_experts[sorted_experts.shape[0]//2]
+    rightmosts = sorted_experts[:ROOM_SIZE//2]
+
+    chosen_experts = np.concatenate((leftmosts, middle, rightmosts), axis=1)
+
+    return chosen_experts, satisfied
 
 def check_conflict(tweet_id):
     query = {"_id": tweet_id}
@@ -90,7 +110,7 @@ def check_conflict(tweet_id):
 
 def sa():
     tweet_id = "1522567873742393344"
-    #check_conflict(tweet_id)
+    # check_conflict(tweet_id)
     X = [6, 7, 7, 12, 13, 13, 15, 16, 19, 22]
     print(z_score(X))
 
@@ -106,5 +126,3 @@ scheduler.start()
 
 if __name__ == '__main__':
     application.run(host="0.0.0.0", port=5000)
-
-
--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
-
+import numpy as np
 
 def date_diff_secs(start, end):
     difference = (end - start)
     return difference.total_seconds()
+
+# class CrossCheck:
+#     def __init__(self, data, data_type):
+#         self.data = data
+#         self.data_type = data_type
+#
+# class ScalarCrossCheck(CrossCheck):
+#
+#     def get_variance(self):
+#         return np.
+
+
