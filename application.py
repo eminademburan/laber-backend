d87b367ada66dcc506965362e204a938426eda6e
Mustafa Hakan Kara <mustafahakankara@hotmail.com>
2022-05-09 01:50:56 +0300

    timedelta dependency

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse
